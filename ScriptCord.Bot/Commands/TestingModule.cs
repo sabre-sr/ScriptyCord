53e184009418ecb00a90b6b9ae80f95ad9fda472
Kornel Domeradzki <korneldomeradzki1@gmail.com>
2022-08-10 15:05:13 +0200

    discord api and autofac setup (#1)
    * startup setup
    * removed old api key
    * fixed autofac

---
+++ ScriptCord.Bot/Commands/TestingModule.cs
@@ -0,0 +1,27 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCord.Bot.Commands
+{
+    public interface ITestingModule
+    {
+        /// <summary>
+        /// Responds to a "ping" message with a "pong".
+        /// Used for checking bot availability
+        /// </summary>
+        /// <returns></returns>
+        public Task PingAsync();
+    }
+    
+    public class TestingModule : ModuleBase<SocketCommandContext>, ITestingModule
+    {
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        public Task PingAsync()
+            => ReplyAsync("Pong!");
+    }
+}
--- ScriptCord.Bot/Program.cs
+++ ScriptCord.Bot/Program.cs
@@ -1,9 +1,59 @@
-﻿namespace ScriptCord.Bot
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using ScriptCord.Bot.Commands;
+
+namespace ScriptCord.Bot
 { 
     class Bot
     {
+        private IContainer Container { get; set; }
+
         public static void Main(string[] args)
+            => new Bot().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
+            Container = SetupAutofac();
+
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var client = scope.Resolve<DiscordSocketClient>();
+                client.Log += LogAsync;
+
+                // TODO: This below could go into the constructor perhaps once the logging is moved to a class
+                scope.Resolve<CommandService>().Log += LogAsync;
+
+                // Do not accidentally upload an API token ;) 
+                await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"), true);
+                await client.StartAsync();
+
+                await scope.Resolve<ICommandHandlingService>().InitializeAsync();
+                await Task.Delay(Timeout.Infinite);
+            }
+        }
+
+        public IContainer SetupAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<AutofacServiceProvider>().As<IServiceProvider>();
+            builder.RegisterType<DiscordSocketClient>().As<DiscordSocketClient>().SingleInstance();
+            builder.RegisterType<CommandService>().As<CommandService>().SingleInstance();
+            builder.RegisterType<HttpClient>().As<HttpClient>();
+
+            builder.RegisterType<TestingModule>().As<TestingModule>();
+            builder.RegisterType<CommandHandlingService>().As<ICommandHandlingService>();
+            return builder.Build();
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            // TODO: normal logger for this
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
         }
     }
 }

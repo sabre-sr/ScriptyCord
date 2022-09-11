c54d9e1b12da635b088a7b2384493102e21058c6
TheSlipper <korneldomeradzki1@gmail.com>
2022-09-11 21:25:21 +0200

    migration fix

--- ScriptCord.Migrations/Core/202208300800-initial-script.sql
+++ ScriptCord.Migrations/Core/202208300800-initial-script.sql
@@ -1,5 +1,7 @@
-﻿create schema scriptcord;
+﻿drop schema if exists scriptycord cascade;
+create schema scriptycord;
 
+drop extension if exists "uuid-ossp";
 create extension "uuid-ossp";
 
 SET TIME ZONE 'UTC';
@@ -8,19 +10,19 @@ SET TIME ZONE 'UTC';
 drop table if exists scriptcord.playlist_entries;
 drop table if exists scriptcord.playlists;
 
-create table scriptcord.playlists (
+create table scriptycord.playlists (
 	id serial primary key not null,
-	guild_id bigint not null,
+	guild_id varchar(20) not null,
 	"name" varchar(80) not null,
 	is_default boolean not null default false,
 	admin_only boolean not null default false
 );
 
-create table scriptcord.playlist_entries (
+create table scriptycord.playlist_entries (
 	id uuid primary key default uuid_generate_v4(),
 	playlist_id integer not null,
 	title varchar(150) not null,
 	source varchar(30) not null,
 	
-	constraint fk_playlist foreign key(playlist_id) references scriptcord.playlists(id)
+	constraint fk_playlist foreign key(playlist_id) references scriptycord.playlists(id)
 );

97ddb954899010a42e109a5617b3af3cc1e7e27d
TheSlipper <korneldomeradzki1@gmail.com>
2022-09-14 10:00:19 +0200

    shuffling algorithms

---
+++ ScriptCord.Core/Algorithms/Shuffling/IShuffle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCord.Core.Algorithms.Shuffling
+{
+    public interface IShuffle<T>
+    {
+        IList<T> Shuffle();
+    }
+}
